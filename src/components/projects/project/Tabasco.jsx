d5766b17ad38f564b23de090a4646dac99c798e7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 19:46:00 +0100

    Create Tabasco project component

---
+++ src/components/projects/project/Tabasco.jsx
@@ -0,0 +1,16 @@
+export function Tabasco() {
+  return (
+    <>
+      <h2>Tabasco</h2>
+      <p>A social media site based on Twitter and Pixiv.</p>
+
+      <h3>Features</h3>
+      <ul>
+        <li>Image uploads</li>
+        <li>Creating/liking posts</li>
+        <li>Adding friends</li>
+        <li>Testing APIs.</li>
+      </ul>
+    </>
+  );
+}
