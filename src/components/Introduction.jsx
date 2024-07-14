86023cead65f24df9191ec418eb6a6f384c8760d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 23:36:50 +0100

    Create Introduction component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,10 @@
+export function Introduction() {
+  return (
+    <>
+      <p>I like bouldering and my blue Patagonia jacket.</p>
+      <p>
+        I'm more experienced with JavaScript but I've recently picked up C#.
+      </p>
+    </>
+  );
+}
