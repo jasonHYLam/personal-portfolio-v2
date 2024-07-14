5930a11ceb85da70a06e296b514e311b4c2333b0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 19:47:33 +0100

    Create BlueMessagingApp

---
+++ src/components/projects/project/BlueMessagingApp.jsx
@@ -0,0 +1,16 @@
+export function BlueMessagingApp() {
+  return (
+    <>
+      <h2>Blue Messaging App</h2>
+      <p>A messaging app based on Discord.</p>
+
+      <h3>Features</h3>
+      <ul>
+        <li>Image uploads</li>
+        <li>Creating chats and groupchats with invited friends</li>
+        <li>Adding friends</li>
+        <li>Testing APIs</li>
+      </ul>
+    </>
+  );
+}
