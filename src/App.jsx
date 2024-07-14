120b63401631aa558350f7c66a39ddcf5f138d3d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 18:57:42 +0100

    Create TechStack component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header></header>
+      {/* Introduction */}
+      <section></section>
+      {/* About me */}
+      <section>
+        <p>I'm Jason, full-stack web dev.</p>
+      </section>
+      {/* Projects */}
+      <section>
+        <article>
+          <p>Saturated</p>
+          <p>
+            An educational art app to help artists improve their observation
+            skills and expand their visual library.
+          </p>
+        </article>
+      </section>
+      {/* Contact me */}
+      <section>
+        <p>I'd love to hear from you.</p>
+        <form action=""></form>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
