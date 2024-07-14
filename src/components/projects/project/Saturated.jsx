e76bb7bff46f85a08ff63d1397de8ce739b68020
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 19:19:37 +0100

    Create Saturated component with title, description and features

---
+++ src/components/projects/project/Saturated.jsx
@@ -0,0 +1,19 @@
+export function Saturated() {
+  return (
+    <>
+      <h2>Saturated</h2>
+      <p>
+        An educational art app to help artists improve their observational
+        skills and expand their visual library.
+      </p>
+
+      <h3>Features</h3>
+      <ul>
+        <li>Image uploads</li>
+        <li>Image analysis</li>
+        <li>Creating notes</li>
+        <li>Tagging notes on image</li>
+      </ul>
+    </>
+  );
+}
