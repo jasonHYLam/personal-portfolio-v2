05cb8f6a388aa950511062ec042dc145108be2ad
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 19:36:52 +0100

    Add Saturated to ProjectsList

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,3 +1,10 @@
+import { Saturated } from "./project/Saturated";
+
 export function ProjectsList() {
-  return <h1>Projects</h1>;
+  return (
+    <>
+      <h1> Projects</h1>;
+      <Saturated />;
+    </>
+  );
 }
