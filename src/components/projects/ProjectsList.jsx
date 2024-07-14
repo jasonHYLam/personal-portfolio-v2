06abe540fbabb64fadf8fee32bf590c0e187daa5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-14 19:48:03 +0100

    Add Tabasco and BlueMessagingApp to ProjectsList

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,10 +1,14 @@
+import { BlueMessagingApp } from "./project/BlueMessagingApp";
 import { Saturated } from "./project/Saturated";
+import { Tabasco } from "./project/Tabasco";
 
 export function ProjectsList() {
   return (
     <>
       <h1>Projects</h1>
       <Saturated />
+      <Tabasco />
+      <BlueMessagingApp />
     </>
   );
 }
