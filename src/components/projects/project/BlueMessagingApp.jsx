6a07ee3eb9fee01aca1b5710c7b0421099754887
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-15 23:22:58 +0100

    Add image to BlueMessagingApp

--- src/components/projects/project/BlueMessagingApp.jsx
+++ src/components/projects/project/BlueMessagingApp.jsx
@@ -1,40 +1,42 @@
 import styles from "./project.module.css";
+import blueMessagingApp1 from "../../../assets/blueMessagingApp1.png";
 
 export function BlueMessagingApp() {
   return (
     <>
       <h2>Blue Messaging App</h2>
-      <section className={styles.projectContainer}></section>
+      <section className={styles.projectContainer}>
+        <img src={blueMessagingApp1} alt="" />
+        <section className={styles.informationContainer}>
+          <p>A messaging app based on Discord.</p>
 
-      <section className={styles.informationContainer}>
-        <p>A messaging app based on Discord.</p>
-
-        <h3>Features</h3>
-        <ul>
-          <li>Image uploads</li>
-          <li>Creating chats and groupchats with invited friends</li>
-          <li>Adding friends</li>
-          <li>Testing APIs using SuperTest, MongoMemoryServer and Jest</li>
-        </ul>
-        <section>
-          <a
-            href="https://blue-messaging-app.netlify.app/login"
-            target="_blank"
-          >
-            Live link
-          </a>
-          <a
-            href="https://github.com/jasonHYLam/TOP-messaging-app-client/"
-            target="_blank"
-          >
-            Frontend repo
-          </a>
-          <a
-            href="https://github.com/jasonHYLam/TOP-messaging-app-backend/"
-            target="_blank"
-          >
-            Backend repo
-          </a>
+          <h3>Features</h3>
+          <ul>
+            <li>Image uploads</li>
+            <li>Creating chats and groupchats with invited friends</li>
+            <li>Adding friends</li>
+            <li>Testing APIs using SuperTest, MongoMemoryServer and Jest</li>
+          </ul>
+          <section>
+            <a
+              href="https://blue-messaging-app.netlify.app/login"
+              target="_blank"
+            >
+              Live link
+            </a>
+            <a
+              href="https://github.com/jasonHYLam/TOP-messaging-app-client/"
+              target="_blank"
+            >
+              Frontend repo
+            </a>
+            <a
+              href="https://github.com/jasonHYLam/TOP-messaging-app-backend/"
+              target="_blank"
+            >
+              Backend repo
+            </a>
+          </section>
         </section>
       </section>
     </>
