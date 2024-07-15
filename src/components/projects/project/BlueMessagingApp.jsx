82cac755d5e04df8721b9e11954ce40b168a18a0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-15 23:17:58 +0100

    Style BlueMessagingApp

--- src/components/projects/project/BlueMessagingApp.jsx
+++ src/components/projects/project/BlueMessagingApp.jsx
@@ -1,32 +1,42 @@
+import styles from "./project.module.css";
+
 export function BlueMessagingApp() {
   return (
     <>
       <h2>Blue Messaging App</h2>
-      <p>A messaging app based on Discord.</p>
+      <section className={styles.projectContainer}></section>
 
-      <h3>Features</h3>
-      <ul>
-        <li>Image uploads</li>
-        <li>Creating chats and groupchats with invited friends</li>
-        <li>Adding friends</li>
-        <li>Testing APIs using SuperTest, MongoMemoryServer and Jest</li>
-      </ul>
+      <section className={styles.informationContainer}>
+        <p>A messaging app based on Discord.</p>
 
-      <a href="https://blue-messaging-app.netlify.app/login" target="_blank">
-        Live link
-      </a>
-      <a
-        href="https://github.com/jasonHYLam/TOP-messaging-app-client/"
-        target="_blank"
-      >
-        Frontend repo
-      </a>
-      <a
-        href="https://github.com/jasonHYLam/TOP-messaging-app-backend/"
-        target="_blank"
-      >
-        Backend repo
-      </a>
+        <h3>Features</h3>
+        <ul>
+          <li>Image uploads</li>
+          <li>Creating chats and groupchats with invited friends</li>
+          <li>Adding friends</li>
+          <li>Testing APIs using SuperTest, MongoMemoryServer and Jest</li>
+        </ul>
+        <section>
+          <a
+            href="https://blue-messaging-app.netlify.app/login"
+            target="_blank"
+          >
+            Live link
+          </a>
+          <a
+            href="https://github.com/jasonHYLam/TOP-messaging-app-client/"
+            target="_blank"
+          >
+            Frontend repo
+          </a>
+          <a
+            href="https://github.com/jasonHYLam/TOP-messaging-app-backend/"
+            target="_blank"
+          >
+            Backend repo
+          </a>
+        </section>
+      </section>
     </>
   );
 }
