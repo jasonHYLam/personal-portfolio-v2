e85c905a65ee541ed07db5565b2b05c9957f4564
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-17 17:48:47 +0100

    Separate about description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,26 @@
+import styles from "./about.module.css";
 import { forwardRef } from "react";
 import { TechStack } from "../techStack/TechStack";
 
 export const About = forwardRef(function About({}, ref) {
   return (
     <>
-      <section ref={ref}>
+      <section className={styles.aboutSection} ref={ref}>
         <h1>About me</h1>
         <p>
-          I'm based in London. I've recently graduated with an upper 2:1
-          Bachelor's Degree in Civil Engineering from the University of Bath. I
-          discovered full-stack web-development through The Odin Project. I have
+          I'm a web developer based in London, and have recently graduated with
+          an upper 2:1 Bachelor's Degree in Civil Engineering from the
+          University of Bath.
+        </p>
+        <p>
+          I discovered full-stack web-development through The Odin Project, have
           a strong growth mindset having developed many web applications and
-          becoming comfortable with previously unfamiliar technologies. I mainly
-          use React and TypeScript for frontend development. After predominantly
-          using JavaScript for backend development, I took the opportunity to
-          learn C# .NET to build my educational web app.
+          becoming comfortable with previously unfamiliar technologies.
+        </p>
+        <p>
+          I mainly use React and TypeScript for frontend. After predominantly
+          using JavaScript and NodeJS for backend development, I took the
+          opportunity to learn C# and .NET to build my educational web app.
         </p>
         <TechStack />
       </section>
