e9ae48102620db0bf72fb3013f85bf04635ada0c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-17 13:09:02 +0100

    Create About section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+export function About() {
+  return (
+    <>
+      <section>
+        <p>
+          I'm based in London. I graduated with an upper 2:1 Bachelor's Degree
+          in Civil Engineering from the University of Bath. Discovered The Odin
+          Project, and have been learning ever since.
+        </p>
+      </section>
+    </>
+  );
+}
