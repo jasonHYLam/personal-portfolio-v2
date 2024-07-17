391e7c0eba629450559700d9a75d99d20d568004
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-17 13:48:35 +0100

    Reduce Introduction text

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -5,18 +5,10 @@ export function Introduction() {
       <section className={styles.introduction}>
         <h2>Hello, I'm</h2>
         <h1 className={`${styles.large} ${styles.accent}`}>Jason Lam.</h1>
-        <p>
-          <h1 className={`${styles.large} ${styles.bold}`}>
-            Full-stack web developer{" "}
-          </h1>
-          based in London.
-        </p>
-        <p className={styles.textbox}>
-          I recently graduated from the University of Bath studying Civil
-          Engineering. I've been learning full-stack web development with The
-          Odin Project. I'm more experienced with JavaScript but I've recently
-          picked up C#.
-        </p>
+        <h1 className={`${styles.large} ${styles.bold}`}>
+          Full-stack web developer{" "}
+        </h1>
+        <p>based in London.</p>
 
         <h2 className={styles.centerAlign}>Check out the things I've done!</h2>
       </section>
