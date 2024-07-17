024bdbcf7a903198630631aaf6bec8ac6cff1c57
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-17 17:58:44 +0100

    Adjust spacing for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,21 +7,23 @@ export const About = forwardRef(function About({}, ref) {
     <>
       <section className={styles.aboutSection} ref={ref}>
         <h1>About me</h1>
-        <p>
-          I'm a web developer based in London, and have recently graduated with
-          an upper 2:1 Bachelor's Degree in Civil Engineering from the
-          University of Bath.
-        </p>
-        <p>
-          I discovered full-stack web-development through The Odin Project, have
-          a strong growth mindset having developed many web applications and
-          becoming comfortable with previously unfamiliar technologies.
-        </p>
-        <p>
-          I mainly use React and TypeScript for frontend. After predominantly
-          using JavaScript and NodeJS for backend development, I took the
-          opportunity to learn C# and .NET to build my educational web app.
-        </p>
+        <section className={styles.aboutDescription}>
+          <p>
+            I'm a web developer based in London, and have recently graduated
+            with an upper 2:1 Bachelor's Degree in Civil Engineering from the
+            University of Bath.
+          </p>
+          <p>
+            I discovered full-stack web-development through The Odin Project,
+            have a strong growth mindset having developed many web applications
+            and becoming comfortable with previously unfamiliar technologies.
+          </p>
+          <p>
+            I mainly use React and TypeScript for frontend. After predominantly
+            using JavaScript and NodeJS for backend development, I took the
+            opportunity to learn C# and .NET to build my educational web app.
+          </p>
+        </section>
         <TechStack />
       </section>
     </>
