2d28ccd737c399fb8e9321d42a91f897d8bbaf5f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-19 17:25:09 +0100

    Remove log

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -5,7 +5,6 @@ export function useObserver(targets) {
   // Note to self: input should be ref. Obtain ref.current within useEffect, as it occurs after initial render.
   useEffect(() => {
     const domElements = targets.map((target) => target.current);
-    console.log(domElements);
     createObserver(domElements);
   }, []);
 }
